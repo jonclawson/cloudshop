bf52bec0f9fb0a293b542798afa81805a5e91141
Jonathan Clawson <jonathanclawson@Jonathans-MacBook-Pro.local>
2026-05-18 13:02:43 -0700

    tailwind

--- pages/vite.config.ts
+++ pages/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import Generouted from '@generouted/react-router/plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     react(),
     Generouted(),
+    tailwindcss(),
   ],
   server: {
     port: 5173,
