d22437e3ab3397644f17a318956f00b83438c3d1
Jonathan Clawson <jonathanclawson@Jonathans-MacBook-Pro.local>
2026-06-24 18:38:21 -0700

    refactor terraform deployment

--- terraform/r2.tf
+++ terraform/r2.tf
@@ -4,19 +4,19 @@ resource "cloudflare_r2_bucket" "uploads" {
   location   = "WNAM"  # Western North America
 }
 
-resource "cloudflare_r2_bucket_cors" "r2_cors" {
-  account_id  = var.account_id
-  bucket_name = cloudflare_r2_bucket.uploads.name
-  rules = [{
-    id              = "AllowPagesApp"
-    allowed = {
-      # origins = [var.site_url] # TODO find url of pages app
-      methods = ["GET", "HEAD"]
-      headers = ["*"]
-    }
-    max_age_seconds = 3600
-  }]
-}
+# resource "cloudflare_r2_bucket_cors" "r2_cors" {
+#   account_id  = var.account_id
+#   bucket_name = cloudflare_r2_bucket.uploads.name
+#   rules = [{
+#     id              = "AllowPagesApp"
+#     allowed = {
+#       # origins = [var.site_url] # TODO find url of pages app
+#       methods = ["GET", "HEAD"]
+#       headers = ["*"]
+#     }
+#     max_age_seconds = 3600
+#   }]
+# }
 
 output "r2_bucket_name" {
   value = cloudflare_r2_bucket.uploads.name
