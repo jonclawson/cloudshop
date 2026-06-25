be988a2b7e7d0254a0be7bb438b28666b0ee4e45
Jonathan Clawson <jonathanclawson@Jonathans-MacBook-Pro.local>
2026-06-25 09:30:09 -0700

    uncomment cloud

--- terraform/cloud.tf
+++ terraform/cloud.tf
@@ -7,34 +7,9 @@ terraform {
     }
   }
 
-  # cloud {}
+  cloud {}
 }
 
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
-
-
-
-# variable "cloudflare_api_token" {
-#   description = "Cloudflare API Token"
-#   type        = string
-#   sensitive   = true
-# }
-
-# variable "account_id" {
-#   description = "Cloudflare Account ID"
-#   type        = string
-# }
-
-# variable "domain" {
-#   description = "Your domain"
-#   type        = string
-#   default     = "cloudshop.example.com"
-# }
-
-# variable "environment" {
-#   description = "Environment (development, production)"
-#   type        = string
-#   default     = "production"
-# }
